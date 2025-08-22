5f8bf27b011c62804783d462b72372d07c2cb997
ApxuTechTop <apxutech@gmail.com>
2025-08-22 19:51:47 +0700

    Add hover plugin

---
+++ hover/hover.view.ts
@@ -0,0 +1,11 @@
+namespace $.$$ {
+	export class $apxu_hover extends $.$apxu_hover {
+		event_show( event?: MouseEvent ) {
+			this.hovered(true)
+		}
+
+		event_hide( event?: MouseEvent ) {
+			this.hovered(false)
+		}
+	}
+}
